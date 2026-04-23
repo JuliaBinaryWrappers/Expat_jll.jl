840b66129c763247f3be191afd3f83610bdc7096
jlbuild <juliabuildbot@gmail.com>
2026-04-23 13:29:50 +0000

    Expat_jll build 2.7.5+0

--- src/Expat_jll.jl
+++ src/Expat_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Expat")
-JLLWrappers.@generate_main_file("Expat", UUID("2e619515-83b5-522b-bb60-26c02a35a201"))
+JLLWrappers.@generate_main_file("Expat", Base.UUID("2e619515-83b5-522b-bb60-26c02a35a201"))
 end  # module Expat_jll
--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libexpat,
-        "lib/libexpat.1.11.1.dylib",
+        "lib/libexpat.1.11.3.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
