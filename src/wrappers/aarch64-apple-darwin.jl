ec33b4eaf4a640457b02b97beade18736f3c5017
jlbuild <juliabuildbot@gmail.com>
2026-09-08 01:59:57 +0000

    Expat_jll build 2.8.4+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libexpat,
-        "lib/libexpat.1.12.3.dylib",
+        "lib/libexpat.1.12.4.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
